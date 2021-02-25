9f881a97c7381f3aaa05c7669009d363afe2c32f
Paul H Graham Jr <pjhg14@gmail.com>
2021-02-25 14:26:30 +0000

    Automatically backed up by Learn

--- lib/find_in_array.rb
+++ lib/find_in_array.rb
@@ -5,8 +5,8 @@ def find_element_index(array, value_to_find)
   found = nil
   counter = 0
 
-  while counter < array.length || found == nil do
-    binding.pry
+  while counter < array.length && found == nil do
+    #binding.pry
     if array[counter] == value_to_find
       found = counter
     end
